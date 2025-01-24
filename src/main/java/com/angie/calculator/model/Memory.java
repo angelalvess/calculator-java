8e3b1a370cc0dd6013c0f3a83536b23e2ebe97af
angelalves <angeltrashangel@gmail.com>
2025-01-23 22:19:53 -0300

    feat: add more logic for calculator project

--- src/main/java/com/angie/calculator/model/Memory.java
+++ src/main/java/com/angie/calculator/model/Memory.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Memory {
 
 
+    private enum TypeCommand {
+        CLEAN, NUMBER, DIVIDE, MULTIPLY, SUBTRACT, SUMM, EQUALS, COMMA;
+    }
+
     private static final Memory instancie = new Memory();
     private final List<MemoryObserver> observers = new ArrayList<>();
 
@@ -28,15 +32,47 @@ public class Memory {
     }
 
 
-    public void processCommand (String value) {
+    public void processCommand (String text) {
 
-        if ("AC".equals(value)) {
+        System.out.println(detectTypeCommand(text));
+        if ("AC".equals(text)) {
             currentText = "";
         } else {
-            currentText += value;
+            currentText += text;
         }
 
         observers.stream().forEach(observer -> observer.changedValue(getCurrentText()));
     }
 
+    private TypeCommand detectTypeCommand (String text) {
+        if (currentText.isEmpty() && text == "0") {
+            return null;
+        }
+
+
+        try {
+            Integer.parseInt(text);
+            return TypeCommand.NUMBER;
+        } catch (NumberFormatException e) {
+            if ("AC".equals(text)) {
+                return TypeCommand.CLEAN;
+            } else if ("/".equals(text)) {
+                return TypeCommand.DIVIDE;
+            } else if ("x".equals(text)) {
+                return TypeCommand.MULTIPLY;
+            } else if ("+".equals(text)) {
+                return TypeCommand.SUMM;
+            } else if ("-".equals(text)) {
+                return TypeCommand.SUBTRACT;
+            } else if (",".equals(text)) {
+                return TypeCommand.COMMA;
+            } else if ("=".equals(text)) {
+                return TypeCommand.EQUALS;
+            }
+        }
+
+        return null;
+    }
+
 }
+
